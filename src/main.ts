d845a8e9130a714b1afd34d2f866613df1d8da51
Harold Diaz <harold.diazv1@gmail.com>
2023-12-20 08:34:26 -0500

    Prefix all endpoint and enable cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    allowedHeaders: '*',
+  });
   await app.listen(9000);
 }
 bootstrap();
