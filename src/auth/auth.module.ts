1bfec1ed53521e21a21c983f433e3335a08c8363
Harold Diaz <harold.diazv1@gmail.com>
2023-12-20 09:14:58 -0500

    Set up passport guard and strategy for auth logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './models/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtGuard } from './guards/jwt.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, JwtGuard, JwtStrategy],
   controllers: [AuthController],
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '3600s' },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
